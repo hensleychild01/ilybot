6d17431d3d8c8e96af1dec11c5c04e1281672833
ilybob <hensleychild01@gmail.com>
2023-11-11 21:10:04 -0500

    rawr

--- commands/player.js
+++ commands/player.js
@@ -1,21 +1,21 @@
-class Sword { 
-    constructor(level=1) {
-        this.damage;
-        switch (level) {
-            case 1:
-                this.damage = 2;
-                break;
-            case 2: 
-                this.damage = 5;
-                break;
-            case 3: 
-                this.damage = 10;
-                break;
-            case 4: 
-                this.damage = 15;
-                break;
-        }
+class Sword {
+  constructor(level = 1) {
+    this.damage;
+    switch (level) {
+      case 1:
+        this.damage = 2;
+        break;
+      case 2:
+        this.damage = 5;
+        break;
+      case 3:
+        this.damage = 10;
+        break;
+      case 4:
+        this.damage = 15;
+        break;
     }
+  }
 }
 
 class Player {
@@ -26,6 +26,6 @@ class Player {
       sword: { level: 1, pointer: new Sword(), durability: 20 },
       bow: { arrows: 20 },
     };
-    this.armor = {level: 1, durability: 30}
+    this.armor = { level: 1, durability: 30 };
   }
 }
